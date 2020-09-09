8101f1737379a0d5126f90752f834a503adecf5c
cauezitto <caue.matheus.granrodeo@gmail.com>
2020-09-09 00:19:12 -0300

    renderizando styled-components do lado do servidor

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,30 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
+    function handleCollectStyles(App) {
+      return props => {
+        return sheet.collectStyles(<App {...props} />);
+      };
     }
+
+    const page = renderPage(App => handleCollectStyles(App));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
